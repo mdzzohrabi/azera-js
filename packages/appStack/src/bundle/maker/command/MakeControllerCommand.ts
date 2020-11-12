55aff88266bd5f8791c78bb435a4c5daab79b3e5
Masoud Zohrabi <mdzzohrabi@gmail.com>
2020-11-12 11:06:45 +0330

    make:controller add as service to config file

--- packages/appStack/src/bundle/maker/command/MakeControllerCommand.ts
+++ packages/appStack/src/bundle/maker/command/MakeControllerCommand.ts
@@ -1,8 +1,9 @@
 import { Inject } from '@azera/container';
-import { camelCase, humanize } from '../../../helper';
+import { copyFileSync, existsSync, mkdirSync, readFileSync, readSync, unlinkSync, writeFileSync } from 'fs';
+import { dirname } from 'path';
+import { humanize, dasherize } from '../../../helper';
 import { Kernel } from '../../../Kernel';
 import { Cli, Command, CommandInfo } from '../../cli';
-import { mkdirSync, existsSync, writeFileSync } from 'fs';
 
 /**
  * Make an http route controller
@@ -13,15 +14,16 @@ export class MakeControllerCommand extends Command {
     name: string = "make:controller <name>";
 
     configure(command: CommandInfo) {
-        command.option('-p --path <path>', 'Route path', '/');
+        command.option('-p --path <path>', 'Route path', '/%name%');
         command.option('-d --dir <directory>', 'Controller directory', '/src/controller');
+        command.option('-b --backup', 'Make backup of current configuration file', false);
     }
 
-    @Inject() async run(kernel: Kernel, cli: Cli, name: string, { path, dir }: { path?: string, dir?: string }): Promise<void> {
+    @Inject() async run(kernel: Kernel, cli: Cli, name: string, { path, dir, backup }: { path?: string, dir?: string, backup?: boolean }): Promise<void> {
 
         let fullName = humanize(name) + 'Controller';
-        dir = kernel.rootDir + ( dir ?? '/src/controller' );
-        let file = dir + '/' + fullName + '.ts';
+        let dirPath = kernel.rootDir + ( dir ?? '/src/controller' );
+        let file = dirPath + '/' + fullName + '.ts';
 
         if (existsSync(file)) {
             cli.error(`Controller file "${file}" already exists`);
@@ -29,19 +31,43 @@ export class MakeControllerCommand extends Command {
         }
 
         // Ensure controller directory exists
-        if (!existsSync(dir)) mkdirSync(dir, { recursive: true });
+        if (!existsSync(dirPath)) mkdirSync(dirPath, { recursive: true });
 
         writeFileSync(
             file,
 `import {Controller, Get, Request, Response, Inject} from '@azera/stack';
 
-@Controller(${path ? "'" + path + "'" : ''})
+@Controller(${path ? "'" + path.replace('%name%', dasherize(name)) + "'" : ''})
 export class ${fullName} {
     @Get('/') @Inject() indexAction(req: Request, res: Response) {
         return 'Index action';
     }
 }
 `);
+
+        if (kernel.container.hasParameter(Kernel.DI_PARAM_CONFIG_FILE)) {
+            let configFile = kernel.resolvePath( kernel.container.getParameter<string>(Kernel.DI_PARAM_CONFIG_FILE) );
+
+            if (configFile.endsWith('.json') && existsSync(configFile)) {
+
+                // Make backup
+                if (backup) {
+                    if (existsSync(configFile + '__backup')) unlinkSync(configFile + '__backup');
+                    copyFileSync(configFile, configFile + '__backup');
+                }
+                
+                let config = JSON.parse(readFileSync(configFile).toString());
+                if (config.services) {
+                    config.services.push( (dir?.replace('src/', '') ?? '') + `/${fullName}` );
+                } else {
+                    config.services = [ (dir?.replace('src/', '') ?? '') + `/${fullName}` ];
+                }
+                writeFileSync(configFile, JSON.stringify(config, null, "\t"));
+            } else {
+                cli.warning(`Configuration file %s not found`, configFile);
+            }
+
+        }
 
         cli.success(`Controller ${fullName} successfuly created in ${file}`);
 
