e4918a36a578361737f4316bbe52cb89e2338c87
Masoud Zohrabi <mdzzohrabi@gmail.com>
2020-11-12 11:08:18 +0330

    stack lock file

---
+++ packages/appStack/tests/bundle/Data.spec.ts
@@ -0,0 +1,20 @@
+import { DataModel, DataField } from "../../src/bundle/data/DataDecorators";
+import { DataManager } from '../../src/bundle/data/DataManager';
+
+describe('DataBundle', () => {
+
+    describe('DataManager', () => {
+    
+        @DataModel()
+        class User {
+            @DataField() firstName!: string;
+        }
+
+        let manager = new DataManager();
+            
+        manager.newConnection('main', `memory://test:hello#123#@localhost:27017/expo`);
+        manager.addModel(`main`, User);
+
+    });
+
+});
